cad63a60e6b3a56162fce2454aec870f9f9586d7
Thomas M <mail@lokalnetz.com>
2026-01-31 12:52:02 +0100

    add useless visitorcounter

--- src/components/WebCounter.tsx
+++ src/components/WebCounter.tsx
@@ -1,26 +1,26 @@
 "use client";
 
 export function WebCounter() {
-  const count = 1337;
+  const count = Math.trunc(Math.random() * 999999) + 1;
 
   // Format leading zeros
   const formattedCount = count.toString().padStart(6, "0");
-  
+
   // Split for each "cell"
   const digits = formattedCount.split("");
 
   return (
     <div className="webcounter-container">
-      <table 
-        border={2} 
-        cellPadding={5} 
-        cellSpacing={0} 
+      <table
+        border={2}
+        cellPadding={5}
+        cellSpacing={0}
         className="webcounter-table"
       >
         <tbody>
           <tr>
-            <td 
-              colSpan={6} 
+            <td
+              colSpan={6}
               className="webcounter-header-cell"
             >
               <span className="webcounter-header-text">
@@ -30,7 +30,7 @@ export function WebCounter() {
           </tr>
           <tr>
             {digits.map((digit, index) => (
-              <td 
+              <td
                 key={index}
                 className="webcounter-digit-cell"
               >
