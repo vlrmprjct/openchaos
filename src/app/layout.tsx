3d67ae7e2c8b8e538211a801f52970695eeaf2ef
Thomas M <mail@lokalnetz.com>
2026-01-31 18:24:01 +0100

    reduce colors to 256, uglify entie page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {children}
+        <svg
+            aria-hidden
+            width='0'
+            height='0'
+            className='absolute'
+        >
+            <filter id='posterize-256'>
+                <feComponentTransfer>
+                    <feFuncR type='discrete' tableValues='0 0.25 0.5 0.75 1' />
+                    <feFuncG type='discrete' tableValues='0 0.25 0.5 0.75 1' />
+                    <feFuncB type='discrete' tableValues='0 0.25 0.5 0.75 1' />
+                </feComponentTransfer>
+            </filter>
+        </svg>
+        <div id='app-root' className='nineties'>
+            {children}
+        </div>
       </body>
     </html>
   );
